f05d7ecb105e8594efa9ca500a77e03ce23f9843
nikolina.mandicheva <nikolina.mandicheva@mentormate.com>
2022-04-07 02:14:28 +0300

    branch main -packed array update - Array Max Item Task Additional Path

--- src/com/company/array/ArrayMaxItem.java
+++ src/com/company/array/ArrayMaxItem.java
@@ -5,6 +5,9 @@ import java.util.Collections;
 public class ArrayMaxItem {
     public static void main(String[] args) {
 
+
+      ArrayMaxItem2();
+
         int[] array={1,2,3,45,4,5};
         int i;
         int max=array[0];
@@ -16,12 +19,17 @@ public class ArrayMaxItem {
                 max=array[i];
             }
             }
-         System.out.println("max array item is "+ max);
+         System.out.println("Max array item is "+ max);
         }
 
-       // static void ArrayMaxItem2 (){
-       // int[] array1={12,34,5,6,23,4};
-        //int max=Collections.max(Arrays.asList(array1));
+       static void ArrayMaxItem2 (){
+        // source: https://www.geeksforgeeks.org/find-max-min-value-array-primitives-using-java/
+           // difference between int and Integer is?
+           // Note: If you use type int while declaring the array you will end up seeing this error: “no suitable method found for min(List<int[]>)”
+
+       Integer[] array1={12,34,51,6,23,4};
+     int max=Collections.max(Arrays.asList(array1));
+           System.out.println("Max Array1 Item is: " + max);}
 
     }
 
