e67b93f3450a12e1b734ce92d266c1d9e1c86c17
nikolina.mandicheva <nikolina.mandicheva@mentormate.com>
2022-04-08 20:58:11 +0300

    branch main -updated package array-new class List Lopp1 completed

--- src/com/company/array/ListLoop1.java
+++ src/com/company/array/ListLoop1.java
@@ -7,25 +7,25 @@ public class ListLoop1 {
 
         int[] list1 = {12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200};
         int[] list2 = new int[list1.length];
-        int j=0 ;
+        int j = 0;
         int i;
 
-        for (i = 0; i < list1.length; i++) {
+        for (i = 0; i < list1.length - 1; i++) {
             int residue = list1[i] % 5;
             if (residue == 0) {
-                         list2[j] = list1[i];
-                         j++;
-                          if (list2[j] > 150) {
-                    //break;
+                list2[j] = list1[i];
+                j++;
+                if (list2[j] > 150) {
+                    break;
                 }
             }
         }
-        for (i = 0; i < list2.length; i++) {
-            System.out.println("Numbers divisible by 5 are: " + list2[i] + "; ");
+
+            for (j = 0; j < list2.length - 1; j++) {
+                System.out.println("Numbers divisible by 5 are: " + list2[j] + "; ");
+            }
+
+
         }
 
-
     }
-
-}
-
