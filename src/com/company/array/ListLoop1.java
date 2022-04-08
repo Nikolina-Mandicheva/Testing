e1f686ef28b7da8d100170066d43b008c246fc91
nikolina.mandicheva <nikolina.mandicheva@mentormate.com>
2022-04-08 21:29:43 +0300

    branch main -updated package array-new class List Loop1 completed

--- src/com/company/array/ListLoop1.java
+++ src/com/company/array/ListLoop1.java
@@ -7,25 +7,30 @@ public class ListLoop1 {
 
         int[] list1 = {12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200};
         int[] list2 = new int[list1.length];
-        int j = 0;
+        int j = 1;
         int i;
 
-        for (i = 0; i < list1.length - 1; i++) {
+        for (i = 0; i <= list1.length - 1; i++) {
             int residue = list1[i] % 5;
             if (residue == 0) {
                 list2[j] = list1[i];
                 j++;
-                if (list2[j] > 150) {
+                 if (list2[j-1] > 150) {
                     break;
+                    //180 will go into the new array, but it will break the For, hence 200 will not go thru the for?
                 }
             }
         }
+        //should convert the array in list,as I need only show not 0 values?
 
-            for (j = 0; j < list2.length - 1; j++) {
-                System.out.println("Numbers divisible by 5 are: " + list2[j] + "; ");
+        System.out.println("Numbers divisible by 5 are: ");
+        for (j = 0; j <= list2.length - 1; j++) {
+            if (list2[j] != 0) {
+                 System.out.print(list2[j] + "; ");
             }
+        }
 
-
+        System.out.println();
         }
 
     }
--- src/com/company/array/ReverseList.java
+++ src/com/company/array/ReverseList.java
@@ -4,17 +4,21 @@ public class ReverseList {
     public static void main(String[] args) {
         int[] list1={10,20,30,40,50};
         int[] newArray=new int[list1.length];
-        int i,j= newArray.length;
+        int i,j= 0;
 
 
-        for (i=list1.length-1;i>=0;i--){
-                  // for(j= 0;j< newArray.length;j++){
-          //newArray[j]=  list1[i];
-               // j++;
+        for (i=list1.length-1;i>=0;i--) {
+            // for(j= 0;j< newArray.length;j++){
+            newArray[j] = list1[i];
+            j++;
+        }
+        System.out.println("Reverted list: ");
 
-               System.out.print(list1[i]);
-               newArray[i]=list1[i];
+                for (j=0;j<= newArray.length-1;j++) {
+                    System.out.print(newArray[j]+ "; ");
+                }
+
             }
         }
-    }
+
 
